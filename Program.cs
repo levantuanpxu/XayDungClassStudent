a3f84f7c43e3c42142847e259b6f8f5e4e9b1a99
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-21 20:45:21 +0700

    XayDungClassStudent
    XayDungClassStudent

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Xay_dung_class_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1, "N. Hoa", "Đông Hà", 30);
+
+            Student student2 = new Student();
+            student2.SetId(2);
+            student2.SetName("Th. Minh Nhật");
+            student2.SetAddress("Huế");
+            student2.SetAge(25);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
